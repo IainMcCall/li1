Corrected correlation and volatility calculation. Added weights option.
Some lines here are included for debugging only, and are commented out.

Signed-off-by: Iain McCall <iainmccall91@gmail.com>

analytics/regression_model.py
@@ -4,6 +4,7 @@ Provides analytic function to run a regression model.
 import logging
 
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 import CONFIG
@@ -53,8 +54,9 @@ def run_regression_model(x, y, train_folds, labels, rf, params):
     ky_hat_all = np.array(ky_hat_all)
     ky_all = np.array(ky_all)
     k_errors = ky_hat_all - ky_all
-    model_losses = {'r2_os': out_of_sample_r2(ky_hat_all, ky_all)}
+    model_losses = {'r2_os': out_of_sample_r2(ky_all, ky_hat_all)}
+    huber_delta = params['test']['huber_delta'] * np.std(ky_all, ddof=1)
     for i in CONFIG.LOSS_METHODS:
-        model_losses[i + '_loss'] = loss_function(k_errors, i, params['test']['huber_delta'])
+        model_losses[i + '_loss'] = loss_function(k_errors, i, huber_delta)
 
     return reg_results, model_losses

analytics/testing.py
@@ -23,11 +23,12 @@ def create_k_folds(x, y, k, shuffle):
         (dict): x, y for each fold with train and test arrays.
     """
     n = len(y)
+    fold_size = int(np.ceil(n / k))
     if shuffle:
         x, y = unison_shuffled_copies(x, y)
     k_folds = {}
     for i in range(k):
-        mask = range(int(i*n/k), int(min((i+1) * n/k, n)))
+        mask = range(i * fold_size, min((i + 1) * fold_size, n))
         k_folds['test_' + str(i+1)] = [x[mask], y[mask]]
         k_folds['train_' + str(i+1)] = [np.delete(x, mask, axis=0), np.delete(y, mask, axis=0)]
     return k_folds
@@ -39,7 +40,7 @@ def loss_function(a, loss_calc, huber_delta=0.0):
 
     Args:
         a (ndarray): Errors between predicted and actual data.
-        loss_calc (str): Loss calculation method. 'mse', 'mae', or 'huber'.
+        loss_calc (str): Loss calculation method. 'mse', 'mae', or 'huber', 'phuber'.
         huber_delta (float): Optional. Huber delta to use.
     Returns:
         (ndarray): Average error.
@@ -50,6 +51,8 @@ def loss_function(a, loss_calc, huber_delta=0.0):
         return np.average(np.abs(a))
     elif loss_calc == 'huber':
         return np.average(np.where(np.abs(a) <= huber_delta, 0.5 * (a ** 2), huber_delta * (np.abs(a) - 0.5 * huber_delta)))
+    elif loss_calc == 'phuber':
+        return np.average((huber_delta ** 2) * ((np.sqrt(1 + (a / huber_delta) ** 2)) - 1))
 
 
 def out_of_sample_r2(y, y_predict):
@@ -62,4 +65,4 @@ def out_of_sample_r2(y, y_predict):
     Returns:
         (float): Out-of-sample R-Squared.
     """
-    return 1 - np.sum(np.square(y_predict - y)) / np.sum(np.square(y - np.mean(y)))
+    return 1 - (np.sum(np.square(y_predict - y)) / np.sum(np.square(y - np.mean(y))))

data_extracts/parse_input_ts.py
@@ -39,9 +39,9 @@ def extract_model_data(infile, fill_method='prev'):
     all_data.index = [datetime.strptime(str(d), '%d/%m/%Y').date() for d in all_data.index]
     all_data.replace(0, np.nan, inplace=True)
     if fill_method == 'prev':
-        all_data = all_data.fillna(method='bfill')
-    elif fill_method == 'next':
         all_data = all_data.fillna(method='ffill')
+    elif fill_method == 'next':
+        all_data = all_data.fillna(method='bfill')
     return all_data
 
 

data_transformations/data_prep.py
@@ -2,7 +2,7 @@
 Provides functions for final data preparation (x, y) to use in the models.
 
 1. Add options to parse model data to remove weeks with a dividend date or earnings announcement from the sample.
-2.
+2. Filter input dates to only include business dates - Also for upstream in extraction.
 """
 import numpy as np
 
@@ -26,9 +26,9 @@ def standardise_array(x, center=True, means=None, stdevs=None):
         (ndarray): Standardisation standard deviations.
     """
     if means is None:
-        means = np.mean(x, axis=0) if center else np.zeros(x.shape[1])
+        means = np.mean(x, ddof=1, axis=0) if center else np.zeros(x.shape[1])
     if stdevs is None:
-        stdevs = np.std(x, axis=0)
+        stdevs = np.std(x, ddof=1, axis=0)
     return (x - means) / stdevs, means, stdevs
 
 

data_transformations/data_returns.py
@@ -1,10 +1,7 @@
 """
 Provides functions to convert levels into returns.
 """
-import os
-
 import numpy as np
-import pandas as pd
 
 
 def ff_return(x, h, ff):
@@ -19,11 +16,11 @@ def ff_return(x, h, ff):
         (ndarray): Output levels.
     """
     if ff == 'log':
-        return np.log(x[:-h] / x[h:])
+        return np.log(x[h:] / x[:-h])
     elif ff == 'relative':
-        return x[:-h] / x[h:] - 1
+        return x[h:] / x[:-h] - 1
     elif ff == 'absolute':
-        return x[:-h] - x[h:]
+        return x[h:] - x[:-h]
 
 
 def convert_levels_to_returns(ts, ff, h):

data_transformations/data_stats.py
@@ -6,31 +6,41 @@ Calculate simple statistics for input data series.
 import numpy as np
 
 
-def stdev(x, df=1):
+def stdev(x, df=1, w=None):
     """
     Calculate standard deviations with n degrees of freedom.
 
     Args:
         x (ndarray): Input time series.
         df (float): Degrees of freedom to use.
+        w (ndarray): Optional. Weight to use per point.
     Returns:
         (ndarray): Standard deviation for a period.
     """
     return np.sqrt(np.sum(np.square(x - np.mean(x))) / (len(x) - df))
 
 
-def correlation(x1, x2):
+def correlation(x1, x2, w=None):
     """
     Calculate standard deviations with n degrees of freedom.
 
     Args:
         x1 (ndarray): Input time series 1.
         x2 (ndarray): Input time series 1.
+        w (ndarray): Optional. Weight to use per point.
     Returns:
         (ndarray): Standard Correlation for a period.
     """
-    return (np.sum(np.square(x1 - np.mean(x1)))) * (np.sum(np.square(x2 - np.mean(x2)))) / \
-           (np.sum((x1 - np.mean(x1)) * (x2 - np.mean(x2))))
+    if w is None:
+        covar = np.sum((x1 - np.mean(x1)) * (x2 - np.mean(x2)))
+        var1 = np.sum(np.square(x1 - np.mean(x1)))
+        var2 = np.sum(np.square(x2 - np.mean(x2)))
+    else:
+        w = w * len(w) / np.sum(w)
+        covar = np.sum(((x1 - np.mean(x1)) * (x2 - np.mean(x2))) * w)
+        var1 = np.sum((np.square(x1 - np.mean(x1)) * w))
+        var2 = np.sum((np.square(x2 - np.mean(x2)) * w))
+    return covar / (np.sqrt(var1) * np.sqrt(var2))
 
 
 def overlapping_vols(returns, p, df):
@@ -48,7 +58,7 @@ def overlapping_vols(returns, p, df):
         rv = returns[v].values
         vv = np.empty(n-p)
         for i in range(n - p):
-            vv[i] = stdev(rv[i:i+p], df)
+            vv[i] = stdev(rv[i+1:i+p+1], df)
         vols[v] = vv
     return vols
 
@@ -62,7 +72,7 @@ def overlapping_correlation(x1, x2, p):
         x2 (ndarray): Matrix of input returns 2.
         p (int): Period to use for overlapping correlations.
     Returns:
-
+        (ndarray): Historical rolling correlations.
     """
     n = len(x1)
     correls = x1[p:].copy()
@@ -70,6 +80,6 @@ def overlapping_correlation(x1, x2, p):
         rv = x1[v].values
         vv = np.empty(n-p)
         for i in range(n - p):
-            vv[i] = correlation(rv[i:i+p], x2[i:i+p])
+            vv[i] = correlation(rv[i+1:i+p+1], x2[i+1:i+p+1])
         correls[v] = vv
     return correls