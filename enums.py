01fe887f28a7c43cf79df6c6102eeadb5831c6ba
Iain McCall <iainmccall91@gmail.com>
2022-12-23 07:49:17 +0000

    Included enums.

---
+++ enums.py
@@ -0,0 +1,11 @@
+"""
+Provides enum classes to use for strings.
+"""
+from enum import Enum
+
+
+class Model(Enum):
+    M1_OLS = 'm1_ols'
+    M2_FRIDGE = 'm2_fridge'
+    M3_LASSO = 'm3_lasso'
+    M4_EL = 'm4_el'
